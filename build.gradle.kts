bdb7615766d112ea00a18de4bae4c676c63a7c4a
Silvio Giebl <silvio.giebl@hivemq.com>
2020-06-06 17:06:52 +0200

    Updated gradle to 6.5 Migrated to gradle kotlin dsl Added gradle.properties Refactored build.gradle.kts

---
+++ build.gradle.kts
@@ -0,0 +1,41 @@
+plugins {
+    id("java-gradle-plugin")
+    id("com.gradle.plugin-publish")
+}
+
+group = "com.github.sgtsilvio.gradle"
+description = "Backports Java 8 APIs (java.util.Optional, java.util.function, java.util.stream, " +
+        "java.util.concurrent.CompletableFuture) to Android APIs < 24 (Android 7.0 Nougat)"
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_1_8
+}
+
+repositories {
+    google()
+    jcenter()
+}
+
+dependencies {
+    implementation("com.android.tools.build:gradle:${property("android.tools.build.version")}")
+    implementation("com.android.tools.build:gradle-api:${property("android.tools.build.version")}")
+    implementation("org.javassist:javassist:${property("javassist.version")}")
+    implementation("org.jetbrains:annotations:${property("annotations.version")}")
+}
+
+gradlePlugin {
+    plugins {
+        create("android-retrofix") {
+            id = "${group}.${name}"
+            displayName = "Android RetroFix"
+            description = project.description
+            implementationClass = "${group}.android.retrofix.RetroFixPlugin"
+        }
+    }
+}
+
+pluginBundle {
+    website = "https://github.com/SgtSilvio/android-retrofix"
+    vcsUrl = "https://github.com/SgtSilvio/android-retrofix"
+    tags = listOf("android", "retrofit", "backport")
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,7 @@
+pluginManagement {
+    plugins {
+        id("com.gradle.plugin-publish") version "${extra["plugin.plugin-publish.version"]}"
+    }
+}
+
+rootProject.name = "android-retrofix"
